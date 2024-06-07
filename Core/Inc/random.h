4fa7c5faa6dff99148d7db68fee36fb84745645e
EnesGurden <enesgurden@hotmail.com>
2024-06-07 15:02:10 +0300

    Application and test code

---
+++ Core/Inc/random.h
@@ -0,0 +1,16 @@
+/*
+ * random.h
+ *
+ *  Created on: 6 Jun 2024
+ *      Author: Yazilim
+ */
+
+#ifndef INC_RANDOM_H_
+#define INC_RANDOM_H_
+#include <stdint.h>
+uint8_t a(uint16_t ledData)
+{
+	return ledData-1;
+}
+
+#endif /* INC_RANDOM_H_ */
